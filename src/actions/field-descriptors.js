ffeafc2cd863ce8f2a37ad2be517dbb60dcaa693
erofeev <erofeev@regenergy.ru>
2019-12-26 13:07:39 +0300

    fix

--- src/actions/field-descriptors.js
+++ src/actions/field-descriptors.js
@@ -19,7 +19,7 @@ export const CampaignSettintsFields  = [
         name: 'msg-template',
         type: 'text',
         required: true, // eslint-disable-next-line
-        defaultValue: '${uk} напоминает вам, что за вашей квартирой по адресу ${adres} числится задолженность за жилищно-коммунальные услуги в размере ${dolg} рублей. Телефон ${tel}',
+        defaultValue: '${uk} напоминает вам, что за вашей квартирой по адресу ${adres} числится задолженность за жилищно-коммунальные услуги в размере ${dolg} рублей. Телефон ',
         desc: 'Шаблон сообщения',
         validator: notEmptyValidator,
     },
